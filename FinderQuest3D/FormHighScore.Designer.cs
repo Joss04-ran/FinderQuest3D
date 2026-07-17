98f63cfdc7e389aa8227370a0d961ca8469523d0
EDBERT FEDERICO <s160425095@microsoft.ubaya.ac.id>
2026-07-17 10:46:24 +0700

    add savedata and readdata

--- FinderQuest3D/FormHighScore.Designer.cs
+++ FinderQuest3D/FormHighScore.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.listBoxDisplay = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // listBoxDisplay
@@ -40,6 +41,19 @@
             this.listBoxDisplay.Size = new System.Drawing.Size(660, 356);
             this.listBoxDisplay.TabIndex = 0;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "All",
+            "Win",
+            "Lose"});
+            this.comboBox1.Location = new System.Drawing.Point(64, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // FormHighScore
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -47,6 +61,7 @@
             this.BackgroundImage = global::FinderQuest3D.Properties.Resources.backgroundQuestion;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.listBoxDisplay);
             this.DoubleBuffered = true;
             this.Name = "FormHighScore";
@@ -59,5 +74,6 @@
         #endregion
 
         private System.Windows.Forms.ListBox listBoxDisplay;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- FinderQuest3D/FormHighScore.cs
+++ FinderQuest3D/FormHighScore.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,18 +14,38 @@ namespace FinderQuest3D
 {
     public partial class FormHighScore : Form
     {
+        string filePath = Path.Combine(Application.StartupPath, "highscores.dat");
+        public List<Players> highScores = new List<Players>();
         public FormHighScore()
         {
             InitializeComponent();
         }
+        private void SaveData(string fileName)
+        {
+            FileStream file = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(file, highScores);
+            file.Close();
+        }
+        private void ReadData(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+                BinaryFormatter formatter = new BinaryFormatter();
+                highScores = (List<Players>)formatter.Deserialize(file);
+                file.Close();
+            }
+        }
 
         private void FormHighScore_Load(object sender, EventArgs e)
         {
-            listBoxDisplay
+            
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
         }
     }
 }
